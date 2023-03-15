4c7bb9e339ed65d3012478c99e5743add3576b89
Satoshi170 <sato1704@outlook.jp>
2023-03-16 04:26:29 +0900

    ヘッダーの内容をログイン中かどうかで内容が変わるようにした。 ヘッダーにドロップダウンメニューを実装した

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable
+
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :password, presence: true, length: { minimum: 6 }
 end
