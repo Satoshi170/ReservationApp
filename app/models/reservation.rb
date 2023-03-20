e984493dab399808193b5faca2241c972c592b32
Satoshi170 <sato1704@outlook.jp>
2023-03-21 07:38:54 +0900

    Roomのformを整えた

---
+++ app/models/reservation.rb
@@ -0,0 +1,24 @@
+class Reservation < ApplicationRecord
+  belongs_to :user
+  belongs_to :room
+
+  validates :checkin_date, presence: true
+  validate :start_check
+  validates :checkout_date, presence: true
+  validate :start_end_check
+  validates :count_people, presence: true
+
+  private
+
+  def start_check
+    if Date.current > checkin_date
+      errors.add(:checkin_date, "は過去の日付を登録できません")
+    end
+  end
+
+  def start_end_check
+    if checkin_date > checkout_date
+      errors.add(:checkout_date, "はチェックイン日より前の日付は登録できません")
+    end
+  end
+end
